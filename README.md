562baabd9fb235895271b3cd892e5b2cba0373fc
PseudoMon <aliya.nisa@yahoo.com>
2022-04-07 13:47:59 +0700

    Minor fixes and readme updated

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Multilingual Genshin Database
 This is the repo containing the source code *and* all the data included in [Multilingual Genshin Database](https://multilingual-genshin.netlify.app). 
 
-You can find all the text data in the `data` directory. Images can be found in `public/images`. Feel free to grab use these data for your needs. Credit back is appreciated but not necessary.
+You can find all text data in the `data` directory. Images can be found in `public/images`. Feel free to grab and use these data for your needs. Credit back is appreciated but not necessary.
 
 The `app` directory contains the source code used to build the site. The site is built with [Remix](https://remix.run) and its current iteration is deployed on [Netlify](https://www.netlify.com/). 
 
-Text data are gathered from [Dimbreath's repository](https://github.com/Dimbreath/GenshinData). Images came from various sources, including [Genshin Impact Wiki on Fandom](https://genshin-impact.fandom.com/), [Paimon.moe](https://paimon.moe), and [Honey Impact](https://genshin.honeyhunterworld.com). All these data came from the live version of Genshin Impact, by Hoyoverse.
+Text data are gathered from [Dimbreath's repository](https://github.com/Dimbreath/GenshinData). Images came from various sources, including [Genshin Impact Wiki on Fandom](https://genshin-impact.fandom.com/), [Paimon.moe](https://paimon.moe), and [Honey Impact](https://genshin.honeyhunterworld.com). All these data came from the live version of Genshin Impact, a game created by Hoyoverse.
 
-This is a fan-project unaffiliated with Hoyoverse, meant primarily for educational purpose. 
+This is a fan-project unaffiliated with Hoyoverse, meant primarily for educational purpose.
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,24 +1,26 @@
 export default function Index() {
   return (
     <main className="homepage">
-      <div class="text">
+      <div className="text">
         <h1>Welcome to Multilingual Genshin Database</h1>
-        <p>This site is a database for <em>text-based</em> data for Genshin Impact where you can easily switch between all the officially-supported languages of the game.</p>
-        <p>We hope this website can be useful for lore-hunters, language-learners, and those interested in video game localization alike!</p>
+        <p>This site is a database for <em>text-based</em> data for Genshin Impact where you can easily switch between all the officially-supported languages of the game. We hope this website can be useful for lore-hunters, language-learners, and those interested in video game localization alike!</p>
         <p>If you're looking for a database focused on stats or other gameplay features, check out <a href="https://paimon.moe">Paimon.moe</a> instead!</p> 
         <p>This is a sister-site to <a href="https://pradewata.netlify.app">Pradewata</a>, a project aiming to retranslate Genshin Impact into Indonesian that retains personality from the source text.  </p>
-        <p>All text data in this website are taken from <a href="https://github.com/Dimbreath/GenshinData">Dimbreath's repository</a>, which contain data from the live version of Genshin Impact. All images came from <a href="https://genshin-impact.fandom.com">Genshin Impact Wiki on fandom</a>.</p>
+        <p>All text data in this website are taken from <a href="https://github.com/Dimbreath/GenshinData">Dimbreath's repository</a>, which contain data from the live version of Genshin Impact. Images come from various sources. Feel free to peruse the source code <a href="https://github.com/PseudoMon/multilingual-genshin/">on GitHub</a>.</p>
       </div>
 
       <div>
-        <div class="text">
+        <div className="text">
           <h2>WIP</h2>
-          <p>This site is currently still a <strong>work in progress</strong>! At the moment we have <strong>Voice Overs</strong> and <strong>Stories</strong> from every character's profile, along with text data for all the <strong>Artifacts</strong>. Next, we are planning to add all Weapon stories.</p>
+          <p>This site is currently still a <strong>work in progress</strong>! At the moment we have <strong>Voice Overs</strong>, <strong>Stories</strong>, and <strong>Talents</strong> from every character's profile and effects/description/lore for every <strong>Artifacts</strong>. Next on the plan is Weapon stories.</p>
           <p>Feel free to spread the word about this site. Also maybe follow the developer at <a href="https://twitter.com/PseudoMonious">@PseudoMonious</a> and drop a comment or two. Always nice to hear something I made isn't just useful for myself.</p>
         </div>
-        <img src="/images/rexincognito.webp"/>
-        <img src="/images/choukenshinkageuchi.webp"/>
+
+        <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
+          <img src="/images/rexincognito.webp"/>
+          <img src="/images/choukenshinkageuchi.webp"/>
+        </div>
       </div>
     </main>
-  );
+  )
 }
