48a36c2ff0fb89fb8ca56daf6a7794a96f97138e
PseudoMon <aliya.nisa@yahoo.com>
2022-05-26 09:25:46 +0700

    Updated the frontpage and such

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -7,13 +7,15 @@ export default function Index() {
         <p>If you're looking for a database focused on stats or other gameplay features, check out <a href="https://paimon.moe">Paimon.moe</a> instead!</p> 
         <p>This is a sister-site to <a href="https://pradewata.netlify.app">Pradewata</a>, a project aiming to retranslate Genshin Impact into Indonesian that retains personality from the source text.  </p>
         <p>All text data in this website are taken from <a href="https://github.com/Dimbreath/GenshinData">Dimbreath's repository</a>, which contain data from the live version of Genshin Impact. Images come from various sources. Feel free to peruse the source code <a href="https://github.com/PseudoMon/multilingual-genshin/">on GitHub</a>.</p>
+
+        <p><i>Currently contain data up to Genshin Version 2.6</i></p>
       </div>
 
       <div>
         <div className="text">
           <h2>WIP</h2>
-          <p>This site is currently still a <strong>work in progress</strong>! At the moment we have <strong>Voice Overs</strong>, <strong>Stories</strong>, and <strong>Talents</strong> from every character's profile and effects/description/lore for every <strong>Artifacts</strong>. Next on the plan is Weapon stories.</p>
-          <p>Feel free to spread the word about this site. Also maybe follow the developer at <a href="https://twitter.com/PseudoMonious">@PseudoMonious</a> and drop a comment or two. Always nice to hear something I made isn't just useful for myself.</p>
+          <p>This site is currently still a <strong>work in progress</strong>! At the moment we have data from every <strong>Character's profile</strong>, <strong>Artifacts</strong>, and <strong>Weapons</strong>. Next we're planning on adding quest dialogues (prioritizing the limited-time events), books, and key items!</p>
+          <p>For updates you can follow <a href="https://twitter.com/GenshinMultiDB">@GenshinMultiDB on Twitter</a> or maybe the developer at <a href="https://twitter.com/PseudoMonious">@PseudoMonious</a>. Feel free to drop a comment or two. Always nice to hear something I made isn't just useful for myself.</p>
         </div>
 
         <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
